90f92e2ddda72b4d146fc8da6a4ed0b3a7b6d9bc
Jos <43651265+JosTheDude@users.noreply.github.com>
2022-06-01 19:19:48 -0400

    Update RandomFallingBlocks.java

--- src/main/java/xyz/joscodes/randomfallingblocks/RandomFallingBlocks.java
+++ src/main/java/xyz/joscodes/randomfallingblocks/RandomFallingBlocks.java
@@ -35,50 +35,54 @@ public class RandomFallingBlocks extends JavaPlugin implements Listener {
 
 	@Override
 	public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
-		if (((args.length == 2 || args.length == 1) && label.equalsIgnoreCase("blocks")) || label.equalsIgnoreCase("randomfallingblocks:blocks")) {
-			if (args[0].equalsIgnoreCase("start") && args.length == 2) {
-				if (!this.isStarted) {
-					Integer rate = Integer.valueOf(0);
-					try {
-						rate = Integer.valueOf(args[1]);
-					} catch (final NumberFormatException exc) {
-						sender.sendMessage(ChatColor.RED + "Invalid number!");
+		if (sender.hasPermission("blocks. *")) {
+			if (((args.length == 2 || args.length == 1) && label.equalsIgnoreCase("blocks")) || label.equalsIgnoreCase("randomfallingblocks:blocks")) {
+				if (args[0].equalsIgnoreCase("start") && args.length == 2) {
+					if (!this.isStarted) {
+						Integer rate = Integer.valueOf(0);
+						try {
+							rate = Integer.valueOf(args[1]);
+						} catch (final NumberFormatException exc) {
+							sender.sendMessage(ChatColor.RED + "Invalid number!");
+						}
+						if (rate.intValue() != 0) {
+							this.isStarted = true;
+							this.task = Integer.valueOf(Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask((Plugin) this, new Runnable() {
+								@Override
+								public void run() {
+									for (final Player p : Bukkit.getOnlinePlayers())
+										RandomFallingBlocks.this.spawnBlock(p);
+								}
+							}, (20 / rate.intValue()), (20 / rate.intValue())));
+							sender.sendMessage(ChatColor.GREEN + "Random Falling Blocks started with a rate of " + ChatColor.GOLD + ChatColor.BOLD + rate + ChatColor.RESET + ChatColor.GREEN + " blocks per second!");
+						}
+					} else if (this.isStarted) {
+						sender.sendMessage(ChatColor.RED + "Random Falling Blocks is already started!");
 					}
-					if (rate.intValue() != 0) {
-						this.isStarted = true;
-						this.task = Integer.valueOf(Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask((Plugin) this, new Runnable() {
-							@Override
-							public void run() {
-								for (final Player p : Bukkit.getOnlinePlayers())
-									RandomFallingBlocks.this.spawnBlock(p);
-							}
-						}, (20 / rate.intValue()), (20 / rate.intValue())));
-						sender.sendMessage(ChatColor.GREEN + "Random Falling Blocks started with a rate of " + ChatColor.GOLD + ChatColor.BOLD + rate + ChatColor.RESET + ChatColor.GREEN + " blocks per second!");
+				} else if (args[0].equalsIgnoreCase("stop")) {
+					if (this.isStarted) {
+						this.isStarted = false;
+						if (this.task != null)
+							Bukkit.getScheduler().cancelTask(this.task.intValue());
+						sender.sendMessage(ChatColor.GREEN + "Random Falling Blocks stopped.");
+					} else if (!this.isStarted) {
+						sender.sendMessage(ChatColor.RED + "Random Falling Blocks not started!");
 					}
-				} else if (this.isStarted) {
-					sender.sendMessage(ChatColor.RED + "Random Falling Blocks is already started!");
+				} else if ((!args[0].equalsIgnoreCase("start") || args.length == 2) && !args[0].equalsIgnoreCase("stop")) {
+					sender.sendMessage(ChatColor.RED + "Invalid Usage. Please try:");
+					sender.sendMessage(ChatColor.GREEN + "/blocks start <blocks per second>");
+					sender.sendMessage(ChatColor.GREEN + "/blocks stop");
 				}
-			} else if (args[0].equalsIgnoreCase("stop")) {
-				if (this.isStarted) {
-					this.isStarted = false;
-					if (this.task != null)
-						Bukkit.getScheduler().cancelTask(this.task.intValue());
-					sender.sendMessage(ChatColor.GREEN + "Random Falling Blocks stopped.");
-				} else if (!this.isStarted) {
-					sender.sendMessage(ChatColor.RED + "Random Falling Blocks not started!");
-				}
-			} else if ((!args[0].equalsIgnoreCase("start") || args.length == 2) && !args[0].equalsIgnoreCase("stop")) {
+			} else if (args.length != 1 && args.length != 2) {
 				sender.sendMessage(ChatColor.RED + "Invalid Usage. Please try:");
 				sender.sendMessage(ChatColor.GREEN + "/blocks start <blocks per second>");
 				sender.sendMessage(ChatColor.GREEN + "/blocks stop");
 			}
-		} else if (args.length != 1 && args.length != 2) {
-			sender.sendMessage(ChatColor.RED + "Invalid Usage. Please try:");
-			sender.sendMessage(ChatColor.GREEN + "/blocks start <blocks per second>");
-			sender.sendMessage(ChatColor.GREEN + "/blocks stop");
+			return true;
 		}
 		return true;
 	}
+
 
 	public void spawnBlock(final Player p) {
 		final Random r = new Random();
